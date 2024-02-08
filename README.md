1edb9beffa191d16a1ec75d15de4fe08baa3dea0
demd7362 <demd7362@gmail.com>
2024-02-09 01:34:23 +0900

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-![image](https://github.com/demd7362/spm-front/assets/115058411/494531ec-1c0d-4e4e-ab30-c3ff7d3cf097)
--- src/main/java/com/app/gobiser/global/config/DBConfig.java
+++
@@ -1,9 +0,0 @@
-package com.app.gobiser.global.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.app.gobiser.domain.*.vo")
-public class DBConfig {
-}
