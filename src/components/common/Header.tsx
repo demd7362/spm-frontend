67f3f8e13f398d9b28a1ca312fc349c8f297142b
demd7362 <demd7362@gmail.com>
2024-02-25 19:51:14 +0900

    fix : header design

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,15 +1,16 @@
 import {ReactElement, useContext, useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import {ContextStore} from "../../router/AppRouter";
+import '../../index.css'
 
 export default function Header() {
     const {header} = useContext(ContextStore);
     return (
-        <div className="bg-black text-white top-0 sticky z-10 w-full">
-            <div className="container mx-auto flex justify-between p-4">
-                <div className="font-bold text-lg space-x-3">
-                    <Link to={'/'}>메인</Link>
-                    <Link to={'/board/1'}>게시판</Link>
+        <div className="bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 text-white top-0 sticky z-10 w-full shadow-lg">
+            <div className="container mx-auto flex justify-between items-center py-4 px-6">
+                <div className="flex space-x-4">
+                    <Link to={'/'} className="hover:underline">메인</Link>
+                    <Link to={'/board/1'} className="hover:underline">게시판</Link>
                     {header.menu}
                 </div>
                 <nav className="space-x-4">{header.authMenu}</nav>
